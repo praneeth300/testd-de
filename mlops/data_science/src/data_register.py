dc6dd6c1f89a7e4e2cde5c5e84c788c0452d6479
Praneeth <64009514+praneeth300@users.noreply.github.com>
2025-05-29 16:41:20 +0530

    Delete mlops directory

--- mlops/data_science/src/data_register.py
+++
@@ -1,9 +0,0 @@
-from huggingface_hub import HfApi
-import os
-
-api = HfApi(token=os.getenv("HF_TOKEN"))
-api.upload_folder(
-    folder_path="data",
-    repo_id="praneeth232/bank-customer-churn",
-    repo_type="dataset",
-)
