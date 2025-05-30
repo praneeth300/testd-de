10e72e929edee5ca609d3f6fe5af9d395f32e1dc
praneeth300 <praneethsanthosh555@gmail.com>
2025-05-30 06:12:52 +0000

    first commit

---
+++ mlops/data_science/src/data_register.py
@@ -0,0 +1,9 @@
+from huggingface_hub import HfApi
+import os
+
+api = HfApi(token=os.getenv("HF_TOKEN"))
+api.upload_folder(
+    folder_path="data",
+    repo_id="praneeth232/bank-customer-churn",
+    repo_type="dataset",
+)
