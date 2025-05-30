398a279fb7ba21ce6b3da7a3937c2ebb7be9d344
Praneeth <64009514+praneeth300@users.noreply.github.com>
2025-05-30 11:49:56 +0530

    Update backend_deployment.py

--- mlops/data_science/deployment/backend_deployment.py
+++ mlops/data_science/deployment/backend_deployment.py
@@ -3,7 +3,7 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="backend",     # the local folder containing your files
+    folder_path="mlops/backend",     # the local folder containing your files
     repo_id="praneeth232/Backend",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
