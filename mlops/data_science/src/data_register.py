4ae5536c0a9c4719de60b8c5871ce2dde2a287b4
Praneeth <64009514+praneeth300@users.noreply.github.com>
2025-06-03 18:06:30 +0530

    Update data_register.py

--- mlops/data_science/src/data_register.py
+++ mlops/data_science/src/data_register.py
@@ -1,7 +1,14 @@
+# Import the create_repo function from the huggingface_hub library
+from huggingface_hub import create_repo
 from huggingface_hub import HfApi
 import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
+
+create_repo("bank-customer-churn",  # Your dataset repo name
+            repo_type="dataset",  # Specify this is a dataset
+            private=False)  # Set to True if it should be private
+
 api.upload_folder(
     folder_path="mlops/data",
     repo_id="praneeth232/bank-customer-churn",
